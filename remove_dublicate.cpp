05af108ec8324447de35905beab3682505b38f6a
Arun <trivediarun446@gmail.com>
2024-06-15 17:39:49 +0530

    done on 15/06/2024

---
+++ remove_dublicate.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+// here I take k integer that is index for new array in which we remove all dublicate element or 
+// contain only unique element 
+// initial value of k is 1 state that the 0 th index of new array has always unique element 
+        int k =1;
+
+        // Here we run a loop to traverse all element in a array 
+        for(int i=1;i<nums.size();i++)
+        {
+// This condition state that the value at privious element is not equal to the present element then 
+// it will store on new array element 
+            if(nums[i-1]!=nums[i])
+            {
+                nums[k]=nums[i];
+                k++; /*for increasing the indec of new array after inserting the unique element */
+            }
+        }
+        return k;
+    }
+};
