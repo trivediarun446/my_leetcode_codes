b86bdc7da3c76bc49cca670c72c1114d8eb30c9e
Arun <trivediarun446@gmail.com>
2024-06-16 09:36:57 +0530

    Done 16/06/2024

---
+++ remove_element.cpp
@@ -0,0 +1,17 @@
+// It is leet code easy problem In this code we remove the element from an array 
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k =0;
+        for(int i =0;i<nums.size();i++)
+        {
+            if (nums[i]!=val)
+            {
+               nums[k]=nums[i];
+               k++;
+            }
+           
+        }
+        return k ;
+    }
+};
